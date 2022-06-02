2a7e202c4a48696033942143b1df4174be7fca94
rana.waqas <rana.waqas@afiniti.com>
2022-06-02 20:54:21 +0500

    Adding maven support Hello world

---
+++ src/main/beryl/App.java
@@ -0,0 +1,18 @@
+package beryl;
+
+/**
+ * Scan the project
+ * Fetch the pom file
+ * get dependencies
+ * check dependency version vulnerabilities
+ * Check latest version of the dependency
+ * Generate a report
+ * Suggest Changes
+ * Make changes.
+ */
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
