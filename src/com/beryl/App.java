3dc4bc6d832cf73abf5310cbbf55add38c62e6b7
rana.waqas <rana.waqas@afiniti.com>
2022-06-03 15:52:05 +0500

    updated some todos

--- src/com/beryl/App.java
+++ src/com/beryl/App.java
@@ -14,12 +14,20 @@ import org.apache.log4j.Logger;
  */
 
 public class App {
-    /* Get actual class name to be printed on */
     static Logger log = Logger.getLogger(App.class.getName());
 
     public static void main(String[] args) {
+        log.debug("======== Starting beryl ========");
         log.info("Find pom.xml");
-
+        // TODO: 6/3/2022 Write a file scanner
+        log.info("parse pom.xml");
+        // TODO: 6/3/2022 write a parser
+        log.info("check dependencies latest versions");
+        // TODO: 6/3/2022 integrate maven central lib
+        // TODO: 6/3/2022 Write Dependencies Analysis Engine.
+        log.info("find diff of the versions of the dependencies");
+        log.info("Find the vulnerabilities of current versions");
+        log.info("Find the vulnerabilities of latest versions");
 
     }
 }
